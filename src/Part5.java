e54f05496bef1e32b254f11c571c28bff6c8ed10
Marin Glavaš <marin.glavas@outlook.com>
2020-06-28 14:59:54 +0200

    While gene search fix

--- src/Part5.java
+++ src/Part5.java
@@ -6,12 +6,14 @@ public class Part5 {
 
         if (startIndex != -1) {
 
-            int currentEnd = dna.indexOf(endCodon);
+            int currentEnd = dna.indexOf(endCodon, startIndex + 3);
 
             while (currentEnd != -1) {
                 if ((currentEnd - startIndex) % 3 == 0) {
                     gene = dna.substring(startIndex, currentEnd + 3);
                     currentEnd = dna.indexOf(endCodon, currentEnd + 3);
+                    
+                    //System.out.println(currentEnd);
                 }
             }
         }
@@ -20,14 +22,17 @@ public class Part5 {
     }
 
     public static void test() {
-        System.out.println("ATGTAA");
+        System.out.println("ATGTAAC");
         System.out.println(findGeneWhile("ATGTAA", "ATG", "TAA"));
 
         System.out.println("TAA");
         System.out.println(findGeneWhile("TAA", "ATG", "TAA"));
 
-        System.out.println("ATGAATTAAAATTAA");
+        System.out.println("ATGAATTAAAATTAAC");
         System.out.println(findGeneWhile("ATGAATTAAAATTAA", "ATG", "TAA"));
+
+        System.out.println("TAAATGGCA");
+        System.out.println(findGeneWhile("TAAATGGCA", "ATG", "TAA"));
     }
     public static void main(String[] args) {
         test();
